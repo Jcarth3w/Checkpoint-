cf8ad070fecb2a0316c629453ae9e78efaf7548f
Jcarth3w <jdcarthew@bsu.edu>
2025-07-18 13:46:05 -0500

    Create App Base

---
+++ Checkpoint!/DomainModel/Game.cs
@@ -0,0 +1,11 @@
+﻿namespace DomainModel;
+
+public class Game
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Studio { get; set; }
+    public string Publisher { get; set; }
+    public string Genre { get; set; }
+
+}
